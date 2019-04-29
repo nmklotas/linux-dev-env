f97c4d82cf786b2ff47242582d0facc9656ff783
Julius <4801422+nmklotas@users.noreply.github.com>
2019-04-29 14:53:41 +0300

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -42,4 +42,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 sudo apt install cifs-utils
-echo 100000 | sudo tee /proc/sys/fs/inotify/max_user_watches
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p

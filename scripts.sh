62a82799104e2ea81b6e3563bf82cb9f12206829
Julius <4801422+nmklotas@users.noreply.github.com>
2026-08-15 13:56:17 +0300

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,2 +1,10 @@
 # yay
-sudo steamos-readonly disable && sudo pacman-key --init && sudo pacman-key --populate archlinux && sudo pacman-key --populate holo && sudo pacman -Syy --needed base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+sudo steamos-readonly disable && 
+  sudo pacman-key --init && 
+  sudo pacman-key --populate archlinux && 
+  sudo pacman-key --populate holo &&
+  sudo pacman -Syy --needed base-devel && 
+  git clone https://aur.archlinux.org/yay-bin.git && 
+  cd yay-bin && makepkg -si
+
+yay -S coolerdash-git
